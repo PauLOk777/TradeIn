43dfb4aa2fe65a8e07e7acdd9b7470b39f1bb676
PauLOk777 <trocjuk2000@gmail.com>
2019-11-12 19:28:15 +0200

    Fixed some errors with eslintrc

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', function(req, res){
-	res.send("TradeIn");
-	res.end();
+app.get('/', function(req, res) {
+    res.send('TradeIn');
+    res.end();
 });
 
-app.listen(PORT, function(){
-	console.log(`>>> Server has been running at port ${PORT}.`);
+app.listen(PORT, function() {
+    console.log(`>>> Server has been running at port ${PORT}.`);
 });
