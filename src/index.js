300b79be9de1c6a051e64f44169108bd0cf5fe32
PauLOk777 <trocjuk2000@gmail.com>
2019-11-15 17:17:52 +0200

    Now server return static file(index.html).

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.send('TradeIn');
-    res.end();
+    res.sendFile(path.join(__dirname, '..', 'public', 'index'));
 });
 
 app.listen(PORT, function() {
