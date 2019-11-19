1cc834dbfa1f5e88cac85e82a0db7706e08d406d
PauLOk777 <trocjuk2000@gmail.com>
2019-11-19 11:28:25 +0200

    Now server response static files on different requests.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,16 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
+
+app.get('/login', function(req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public','SignIn.html'));
+});
+
+app.get('/registration', function(req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public','regist.html'));
+})
 
 app.listen(PORT, function() {
     console.log(`>>> Server has been running at port ${PORT}.`);
