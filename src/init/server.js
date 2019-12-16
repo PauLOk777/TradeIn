f16d54246d83c95a21349685bc26742d96b2a307
PauLOk777 <trocjuk2000@gmail.com>
2019-12-16 18:07:26 +0200

    Modified bad PORT, added link for chat.

--- src/init/server.js
+++ src/init/server.js
@@ -8,7 +8,7 @@ const engine = require('../engine/engine');
 const router = require('../router/router');
 
 // Constants
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const init = async function() {
     const app = express();
